c7a752a768237c74aa8b908f6190f4979ac83d9a
minusthebear <matt.hamann1982@gmail.com>
2019-07-11 18:43:19 -0700

    AuthContext

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -2,6 +2,11 @@ import auth0 from 'auth0-js';
 
 const REDIRECT_ON_LOGIN = 'redirect_on_login';
 
+let _idToken = null;
+let _accessToken = null;
+let _scopes = null;
+let _expiresAt = null;
+
 export default class Auth {
     constructor(history) {
         this.history = history;
@@ -43,33 +48,19 @@ export default class Auth {
     }
 
     setSession = authResult => {
-        const expiresAt = JSON.stringify(
-            authResult.expiresIn * 1000 + new Date().getTime()
-        );
+        _expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
 
-        const scopes = authResult.scope || this.requestedScopes || '';
+        _scopes = authResult.scope || this.requestedScopes || '';
 
-        console.log(authResult.scope);
-        console.log(this.requestedScopes);
-
-        localStorage.setItem('access_token', authResult.accessToken);
-        localStorage.setItem('id_token', authResult.idToken);
-        localStorage.setItem('expires_at', expiresAt);
-        localStorage.setItem('scopes', JSON.stringify(scopes));
+        _accessToken = authResult.accessToken;
+        _idToken = authResult.idToken;
     }
 
     isAuthenticated() {
-        const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
-        return new Date().getTime() < expiresAt;
+        return new Date().getTime() < _expiresAt;
     }
 
     logout = () => {
-        localStorage.removeItem('access_token');
-        localStorage.removeItem('id_token');
-        localStorage.removeItem('expires_at');
-        localStorage.removeItem('scopes');
-        this.userProfile = null;
-
         this.auth0.logout({
             clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
             returnTo: 'http://localhost:3000'
@@ -77,12 +68,10 @@ export default class Auth {
     }
 
     getAccessToken = () => {
-        const accessToken = localStorage.getItem('access_token');
-
-        if (!accessToken) {
+        if (!_accessToken) {
             throw new Error('No access token found');
         }
-        return accessToken;
+        return _accessToken;
 
     }
 
